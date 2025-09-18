9e2b7934b516e9beaefd3660d579397135f87e1d
Saadetatabay <etatabaysaad143@gmail.com>
2025-09-18 13:05:43 +0300

    ft_range fixed

--- ft_range.c
+++ ft_range.c
@@ -3,10 +3,22 @@ int     *ft_range(int start, int end)
 {
     int i;
     int *array;
+    int temp;
+    i =0;
+    if (start>end)
+    {
+        array = (int *)malloc((start-end+1)*(sizeof(int)));
+        while(start>=end)
+        {
+            array[i] = start;
+            start--;
+            i++;
+        }
+        return(array);
+    }
     array = (int *)malloc((end-start+1)*(sizeof(int)));
     if(!array)
         return NULL;
-    i =0;
     while(start<=end)
     {
         array[i] = start;
@@ -19,5 +31,5 @@ int     *ft_range(int start, int end)
 int main()
 {
 
-    printf("%d %d %d",ft_range(-1,1)[0],ft_range(-1,1)[1],ft_range(-1,1)[2]);
+    printf("%d %d %d",ft_range(0,-2)[0],ft_range(0,-2)[1],ft_range(0,-2)[2]);
 }
